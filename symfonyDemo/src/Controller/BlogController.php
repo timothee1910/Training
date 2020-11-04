73b49ea5b7d65f26b7bca682e381697773cde184
Timothée <ong.timothee.cy@gmail.com>
2020-11-04 14:15:01 +0100

    second commit
    Signed-off-by: Timothée <ong.timothee.cy@gmail.com>

---
+++ symfonyDemo/src/Controller/BlogController.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\ArticleLike;
+use App\Entity\Comment;
+use App\Form\ArticleType;
+use App\Form\CommentType;
+use App\Repository\ArticleLikeRepository;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index() // pour remplacer $repo on peut mettre en param: ArticleRepository $repo et en use : App\Repository\ArticleRepository;
+    {
+        // Pour faire une SELECTION et non une manipulation -> Repository
+        //Pour faire des manipulation ->Manager
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        /**
+         * Exemple $article=$repo->find(12);
+         * $articles = $repo->findBy(['title'=>'titre de ']);
+         *              -> SELECT t0.id AS id_1,
+         *                        t0.title AS title_2,
+         *                        t0.content AS content_3,
+         *                        t0.image AS image_4,
+         *                        t0.created_at AS created_at_5
+         *                 FROM article t0
+         *                 WHERE t0.title = 'titre de '
+         */
+        $articles = $repo->findAll();
+        return $this->render('blog/index.html.twig', ['articles' => $articles]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function home() {
+        return $this->render('blog/home.html.twig', ['title' => "Bienvenue sur ce test", 'age' => 21]);
+    }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     * $article est null quand on en crée un (étape avant la soumission)
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function form(Article $article = null, Request $request, ObjectManager $manager) {
+
+        if (!$article) {
+            $article = new Article();
+        }
+
+        //$form = $this->createFormBuilder($article)
+        //              ->add('title', TextType::class, [
+        //                  'attr' => [
+        //                      'placeholder' => "Titre de l'article"
+        //                  ]
+        //              ])
+        //              ->add('content', TextareaType::class, [
+        //                  'attr' => [
+        //                      'placeholder' => "Contenu de l'article"
+        //                  ]
+        //              ])
+        //              ->add('image',TextType::class, [
+        //                  'attr' => [
+        //                      'placeholder' => "Image de l'article"
+        //                  ]
+        //              ])
+        //             ->getForm();
+        ///!\ a bien lié la variable qui doit contenir les infos sql (l'entité) ici $article avec l'entité Article
+        $form = $this->createForm(ArticleType::class, $article);
+        /**
+         * Analyse de la requète d'envoie du formulaire
+         * Utilisation de la function handleRequest
+         */
+        $form->handleRequest($request);
+        /**
+         * Soumission du formulaire
+         * Si l'urilisateur à appuyer sur Enregistrer
+         * On ajoute une date de création
+         */
+        if ($form->isSubmitted() && $form->isValid()) {
+            //pour la soumission de l'édition = si il a pas d'id déja renseigné
+            //dump($article);
+            if (!$article->getId()) {
+                $article->setCreatedAt(new \DateTime());
+            }
+
+            $manager->persist($article);
+            $manager->flush();
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig', ['formArticle' => $form->createView(), 'editMode' => $article->getId() !== null // param pour changé le bouton. Si il a déja un Id ou non par un booleen
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show($id, Request $request, ObjectManager $manager) {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $article = $repo->find($id);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //On associe le commentaire a l'article
+            $comment->setCreatedAt(new \DateTime())->setArticle($article);
+            $manager->persist($comment);
+            $manager->flush();
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+        return $this->render('blog/show.html.twig', ['article' => $article, 'commentForm' => $form->createView()]);
+    }
+    /**
+     * Peut être écrit avec ConvertParam (il comprend que l'on veut une identification par l'id)
+     * @Route("/blog/{id}", name="blog_show")
+     *
+     *
+     * public function show(Article $article) {
+     *   return $this->render('blog/show.html.twig',[
+     *   'article' => $article
+     *   ]);
+     *   }
+     *
+     */
+
+    /**
+     * Permet de like ou d'unliker un article
+     * Sous format JSON
+     * @Route("blog/{id}/like", name="article_like")
+     *
+     * @param Article               $article
+     * @param ObjectManager         $manager
+     * @param ArticleLikeRepository $likeRepository
+     *
+     * @return Response
+     */
+    public function like(Article $article, ObjectManager $manager, ArticleLikeRepository $likeRepository): Response {
+        //On recupère l'utilisateur
+        $user = $this->getUser();
+        //Si pas connecté
+        if (!$user) {
+            return $this->json(['code' => 403, 'message' => 'Unauthorized'], 403);
+        }
+        //Cet article est-il liké = Si oui pouvoir supprimer ce like
+        // Ou cet article n'est pas liké = Si oui pouvoir liker cet article
+        if ($article->isLikedByUser($user)) {
+            //Si j'aime cet article alors je veux pouvoir supprimer mon'j'aime'
+            //Pour retrouver le like il faut appeler le Repository (en fonction de l'article et de l'utilisateur)
+            $like = $likeRepository->findOneBy(['article' => $article, 'user' => $user]);
+            $manager->remove($like);
+            $manager->flush();
+            return $this->json(['code' => 200, 'message' => 'success pour la suppression du like',
+                                'likes' => $likeRepository->count(['article'=>$article])
+                                ],200);
+        }
+        //Création d'un nouveau like
+        $like = new ArticleLike();
+        $like->setArticle($article)
+            ->setUser($user)
+            ->setCreatedAt(new \DateTime());
+        $manager->persist($like);
+        $manager->flush();
+        return $this->json(['code' => 200,
+                            'message' => 'success le like est bien ajouté',
+                            'likes' => $likeRepository->count(['article' => $article])
+                           ],200);
+    }
+}
---
+++ symfonyDemo/src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Comment;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Article;
+use Faker\Factory;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+        // Creation de 3 catégorie faker
+        for ($i=1; $i<=3; $i++) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                    ->setDescription($faker->paragraph());
+            $manager->persist($category);
+            // Pour chaque catégory crées On crée entre 4 et 6 article
+            for ($j = 1; $j <= mt_rand(4,6); $j++) {
+                $article = new Article();
+                $content = '<p>' .join($faker->paragraphs(3),'</p><p>'). '</p>';
+                $article->setTitle($faker->sentence())
+                    ->setContent($content)
+                    ->setImage($faker->imageUrl())
+                    ->setCreatedAt($faker->dateTimeBetween('-6 month','now'))
+                    ->setCategory($category);
+                $manager->persist($article);
+
+                for ($k = 1; $k<= mt_rand(4, 10); $k++) {
+                    $comment =  new Comment();
+                    $content = '<p>' .join($faker->paragraphs(2),'</p><p>'). '</p>';
+                    $now = new \DateTime();
+                    $interval = $now->diff($article->getCreatedAt());
+                    //pour connaitre le nombre de jour de l'interval
+                    $days = $interval->days;
+                    $min = '-'.$days.'days'; //-100days par exemple Cela nous donne la date de création (en jours) de l'article
+
+                    $comment->setAuthor($faker->name())
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween($min)) //mettre une date entre il y a x jours et maintenant
+                            ->setArticle($article);
+                    $manager->persist($comment);
+                }
+            }
+        }
+        //permet l'insertion dans la bdd après le persist
+        $manager->flush();
+    }
+}
